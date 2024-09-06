3a53af20fa76e760826545fcdbaaff64c99ab605
Frostbit912 <97480950+JonathanBooker912@users.noreply.github.com>
2024-09-06 13:15:59 -0500

    Removed Encoding From Template

--- app/controllers/support/emailSignedForm.js
+++ app/controllers/support/emailSignedForm.js
@@ -7,53 +7,40 @@ import sendMail from "../../utilities/sendMail.js";
 const getEmailBody = (playerName) => {
   const body = `
   <p>Dear ${playerName},</p>
-<p>Attached to this email, you will find the signed copy of your player agreement. We&#39;re exc
-ited to have you on board and look forward to a successful year together. 
-Please take a moment to review the document if you have not already, 
-and don't hesitate to reach out if you have any questions or need any clarifications. 
-Looking forward to working with you!</p>
 
-<p>Blessings,</p>
+  <p  style="text-indent: 25px; padding-left: 25px">Attached to this email, you will find the signed copy of your player agreement. We're excited to have you on board and look forward to a successful year together. Please take a moment to review the document if you have not already, and don't hesitate to reach out if you have any questions or need any clarifications. Looking forward to working with you!</p>
 
-<div>
-      <div dir=3D"ltr" class=3D"gmail_signature" data-smart=
-         mail=3D"gmail_signature">
-         <div dir=3D"ltr">
-            <table border=3D"0" cellspacing=
-            =3D"0" cellpadding=3D"0" style=3D"border-width:0px;border-color:rgb(249,249=
-            ,249);border-style:solid;border-collapse:collapse;color:rgb(128,19,40);font=
-            -family:sofia-pro,-apple-system,BlinkMacSystemFont,&quot;Helvetica Neue&quo=
-            t;,Arial,&quot;Noto Sans&quot;,sans-serif;font-size:18px;margin-top:50px">
-            <tbody style=3D"border-width:0px;border-color:rgb(249,249,249);border-style:=
-            solid">
-            <tr style=3D"border-width:0px;border-color:rgb(249,249,249);border-s=
-               tyle:solid">
-               <td width=3D"88px;" valign=3D"middle" style=3D"border-width:0px=
-               2px 0px 0px;border-color:rgb(249,249,249) rgb(128,19,40) rgb(249,249,249) =
-               rgb(249,249,249);border-style:solid;border-collapse:collapse;width:88px;ver=
-               tical-align:middle;padding:9px 16px 9px 0px"><img src=3D"https://ddtjogezxr=
-               16i.cloudfront.net/images/email/oc-logo-email-75.png" alt=3D"Oklahoma Chris=
-               tian University" width=3D"150" height=3D"150" style=3D"font-family:sofia-pr=
-               o;font-size:17px;font-weight:600;margin:0px;padding:0px;display:block;width=
-               :150px;height:150px"></td><td style=3D"border-width:0px;border-color:rgb(24=
-               9,249,249);border-style:solid;border-collapse:collapse;font-size:12px;line-=
-               height:1.4;font-family:arial,sans-serif;padding-left:14px;padding-top:2px">
-               <div style=3D"border-width:0px;border-color:rgb(249,249,249);border-style:s=
-                  olid;font-weight:bold"><span style=3D"border-width:0px;border-color:rgb(249=
-                  ,249,249);border-style:solid">Lucas Hayworth, MBA</span>
-               </div>
-               <div style=3D=
-               "border-width:0px;border-color:rgb(249,249,249);border-style:solid">Oklahoma Christian University
-         </div>
-         <div style=3D"border-width:0px;border-color:rgb=
-         (249,249,249);border-style:solid;white-space:pre-line"><span style=3D"borde=
-         r-width:0px;border-color:rgb(249,249,249);border-style:solid">Director of Esports | Athletics</span>
+  <p>Blessings,</p>
+
+  <div>
+    <div dir="ltr" class="gmail_signature" data-smartmail="gmail_signature">
+      <div dir="ltr">
+        <table border="0" cellspacing="0" cellpadding="0" style="border-width:0px;border-color:rgb(249,249,249);border-style:solid;border-collapse:collapse;color:rgb(128,19,40);font-family:sofia-pro,-apple-system,BlinkMacSystemFont,'Helvetica Neue',Arial,'Noto Sans',sans-serif;font-size:18px;margin-top:50px">
+          <tbody style="border-width:0px;border-color:rgb(249,249,249);border-style:solid">
+            <tr style="border-width:0px;border-color:rgb(249,249,249);border-style:solid">
+              <td width="88px;" valign="middle" style="border-width:0px 2px 0px 0px;border-color:rgb(249,249,249) rgb(128,19,40) rgb(249,249,249) rgb(249,249,249);border-style:solid;border-collapse:collapse;width:88px;vertical-align:middle;padding:9px 16px 9px 0px">
+                <img src="https://ddtjogezxr16i.cloudfront.net/images/email/oc-logo-email-75.png" alt="Oklahoma Christian University" width="150" height="150" style="font-family:sofia-pro;font-size:17px;font-weight:600;margin:0px;padding:0px;display:block;width:150px;height:150px">
+              </td>
+              <td style="border-width:0px;border-color:rgb(249,249,249);border-style:solid;border-collapse:collapse;font-size:12px;line-height:1.4;font-family:arial,sans-serif;padding-left:14px;padding-top:2px">
+                <div style="border-width:0px;border-color:rgb(249,249,249);border-style:solid;font-weight:bold">
+                  <span style="border-width:0px;border-color:rgb(249,249,249);border-style:solid">Lucas Hayworth, MBA</span>
+                </div>
+                <div style="border-width:0px;border-color:rgb(249,249,249);border-style:solid">Oklahoma Christian University</div>
+                <div style="border-width:0px;border-color:rgb(249,249,249);border-style:solid;white-space:pre-line">
+                  <span style="border-width:0px;border-color:rgb(249,249,249);border-style:solid">Director of Esports | Athletics</span>
+                </div>
+                <span style="border-width:0px;border-color:rgb(249,249,249);border-style:solid">405.305.2692</span> | 
+                <a href="https://discord.gg/dexz9Ev" target="_blank">Discord</a> &amp; 
+                <a href="https://twitter.com/Maestraeux" target="_blank">Twitter</a> @Maestraeux
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <br>
       </div>
-      <span style=3D"border-width:0px;border-color=
-      :rgb(249,249,249);border-style:solid">405.305.2692</span> | <a href=3D=
-      "https://discord.gg/dexz9Ev" target=3D"_blank">Discord</a> &amp; <a hr=
-         ef=3D"https://twitter.com/Maestraeux" target=3D"_blank">Twitter</a> @Maestraeux</td></tr></tbody></table><br>
-   </div>
+    </div>
+  </div>
+
   `;
 
   return body;
